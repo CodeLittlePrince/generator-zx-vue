3a3774a8398cae9be7e84cf3749f828371fba71e
倪俊杰 <1006312908@qq.com>
2018-05-18 00:53:00 +0800

    子咻的脚手架

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-zx-vue [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> a vue scaffolding producted by zixiu
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-zx-vue using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-zx-vue
+```
+
+Then generate your new project:
+
+```bash
+yo zx-vue
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [子咻](https://github.com/CodeLittlePrince)
+
+
+[npm-image]: https://badge.fury.io/js/generator-zx-vue.svg
+[npm-url]: https://npmjs.org/package/generator-zx-vue
+[travis-image]: https://travis-ci.org/CodeLittlePrince/generator-zx-vue.svg?branch=master
+[travis-url]: https://travis-ci.org/CodeLittlePrince/generator-zx-vue
+[daviddm-image]: https://david-dm.org/CodeLittlePrince/generator-zx-vue.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/CodeLittlePrince/generator-zx-vue
---
+++ generators/app/index.js
@@ -0,0 +1,79 @@
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
+const path = require('path')
+const fs = require('fs')
+
+module.exports = class extends Generator {
+  initializing() {
+    // 打印欢迎语
+    this.log(
+      yosay(`Welcome to the shining ${chalk.cyan('generator-zx-vue')} generator!`)
+    )
+  }
+  prompting() {
+    // 让用户选择是否需要包含vuex
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name of project:',
+        default: path.basename(process.cwd())
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Description:',
+        default: '',
+      },
+      {
+        type: 'confirm',
+        name: 'includeVuex',
+        message: 'Would you like to include Vuex in your project?',
+        default: false,
+      }
+    ]
+    return this.prompt(prompts).then(answers => {
+      this.name = answers.name
+      this.description = answers.description
+      this.includeVuex = answers.includeVuex
+      this.log(chalk.green('name: ', this.name))
+      this.log(chalk.green('description: ', this.description))
+      this.log(chalk.green('includeVuex: ', this.includeVuex))
+    })
+  }
+
+  writing() {
+    // 复制普通文件
+    // https://github.com/sboudrias/mem-fs-editor
+    this.fs.copy(
+      this.templatePath(),
+      this.destinationPath(),
+      { globOptions:
+        {
+          dot: true
+        }
+      }
+    )
+    // 根据用户选择，决定是否安装vuex
+    if (this.includeVuex) {
+      const pkgJson = {
+        name: this.name,
+        description: this.description,
+        dependencies: {
+          vuex: '^3.0.1'
+        }
+      }
+      // Extend or create package.json file in destination path
+      this.fs.extendJSON(this.destinationPath('package.json'), pkgJson)
+    }
+  }
+
+  install() {
+    this.npmInstall()
+  }
+
+  end() {
+    this.log(chalk.green('Construction completed!'))
+  }
+}
