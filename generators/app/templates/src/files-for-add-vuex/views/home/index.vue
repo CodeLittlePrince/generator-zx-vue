f19393a9186b9647bac158695d37708494bbb8c0
倪俊杰 <1006312908@qq.com>
2018-06-04 18:20:41 +0800

    将proxy的设置为'/'根 & 通过mock-switch来设置运行时候的成功、错误或其它类型的返回

--- generators/app/templates/src/files-for-add-vuex/views/home/index.vue
+++ generators/app/templates/src/files-for-add-vuex/views/home/index.vue
@@ -15,7 +15,6 @@
 <script>
   import axios from 'axios'
   import mixinsSample from 'mixins/sample'
-  import homeApi from 'api/home'
   import { mapGetters, mapActions } from 'vuex'
 
   export default {
@@ -27,13 +26,23 @@
     },
     mounted() {
       // ajax get data
-      axios.get(homeApi.hello)
+      axios.get('/home/hello', { params: { page: 7 } })
+      // axios.post('/home/hello', { page: 7 }) // FOR POST
         .then(res => {
           const data = res.data
           console.log(`%c${data.msg}`, 'color: blue')
         })
-        .catch(() => {
-          console.warn('%c少年，是不是忘了npm run mock?', 'color: cadetblue')
+        .catch(e => {
+          console.error(e)
+        })
+      // ajax get data
+      axios.get('/home/kitty')
+        .then(res => {
+          const data = res.data
+          console.log(`%c${data.msg}`, 'color: blue')
+        })
+        .catch(e => {
+          console.error(e)
         })
       // vuex action
       this.ageIncrease()
@@ -49,5 +58,41 @@
 </script>
 
 <style lang="scss">
-  @import './style.scss';
+  @import '~common/scss/variable';
+  @import '~common/scss/mixin';
+
+  .home {
+    .content {
+      position: relative;
+      width: 1200px;
+      height: 400px;
+      margin: 0 auto;
+      animation-duration: 1s;
+      h1 {
+        position: absolute;
+        bottom: 30px;
+        left: 120px;
+        font-size: $font-size-amazing;
+        text-align: center;
+        margin-top: 50px;
+        color: $color-text-gray;
+        font-weight: bolder;
+        span {
+          font-size: $font-size-xl;
+        }
+      }
+      .cover {
+        position: absolute;
+        z-index: 2;
+        right: 62px;
+        bottom: 90px;
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        overflow: hidden;
+        @include bg-image('~common/img/home');
+        background-size: contain;
+      }
+    }
+  }
 </style>
