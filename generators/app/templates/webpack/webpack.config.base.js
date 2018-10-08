25545e1107348e0a498ea134a1cf735c890158a6
倪俊杰 <1006312908@qq.com>
2018-10-08 10:49:13 +0800

    fix

--- generators/app/templates/webpack/webpack.config.base.js
+++ generators/app/templates/webpack/webpack.config.base.js
@@ -30,6 +30,9 @@ function resolve(dir) {
 // 网站图标配置
 const favicon = resolve('favicon.ico')
 
+// 指定以__base64为后缀的svg转为base64
+const svgBase64Reg = /__base64\.(svg)(\?.*)?$/
+
 // __dirname: 总是返回被执行的 js 所在文件夹的绝对路径
 // __filename: 总是返回被执行的 js 的绝对路径
 // process.cwd(): 总是返回运行 node 命令时所在的文件夹的绝对路径
