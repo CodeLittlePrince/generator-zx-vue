e950620315b5866eb2076aa5cd974356f29a4a85
倪俊杰 <1006312908@qq.com>
2018-10-09 14:56:40 +0800

    加eslint-plugin-vue限制html格式

--- generators/app/templates/src/views/home/index.vue
+++ generators/app/templates/src/views/home/index.vue
@@ -5,7 +5,9 @@
         {{ `Vue` | filterSample('之脚手架') }}
         <span>- by 咻</span>
       </h1>
-      <a href="https://github.com/CodeLittlePrince/blog" target="__blank">
+      <a
+        href="https://github.com/CodeLittlePrince/blog"
+        target="__blank">
         <div class="cover"></div>
       </a>
     </div>
@@ -13,68 +15,68 @@
 </template>
 
 <script>
-  import mixinsSample from 'mixins/sample'
+import mixinsSample from 'mixins/sample'
 
-  export default {
-    mixins: [mixinsSample],
-    mounted() {
-      // ajax get data
-      this.$ajax.get('/home/hello', { params: { page: 7 } })
-      // this.$ajax.post('/home/hello', { page: 7 }) // FOR POST
-        .then(res => {
-          console.log(`%c${res}`, 'color: blue')
-        })
-        .catch(e => {
-          console.error(e)
-        })
-      // ajax get data
-      this.$ajax.get('/home/kitty')
-        .then(res => {
-          console.log(`%c${res}`, 'color: blue')
-        })
-        .catch(e => {
-          console.error(e)
-        })
-    }
+export default {
+  mixins: [mixinsSample],
+  mounted() {
+    // ajax get data
+    this.$ajax.get('/home/hello', { params: { page: 7 } })
+    // this.$ajax.post('/home/hello', { page: 7 }) // FOR POST
+      .then(res => {
+        console.log(`%c${res}`, 'color: blue')
+      })
+      .catch(e => {
+        console.error(e)
+      })
+    // ajax get data
+    this.$ajax.get('/home/kitty')
+      .then(res => {
+        console.log(`%c${res}`, 'color: blue')
+      })
+      .catch(e => {
+        console.error(e)
+      })
   }
+}
 </script>
 
 <style lang="scss">
-  @import '~common/scss/variable';
-  @import '~common/scss/mixin';
+@import '~common/scss/variable';
+@import '~common/scss/mixin';
 
-  .home {
-    .content {
-      position: relative;
-      width: 1200px;
-      height: 400px;
-      margin: 0 auto;
-      animation-duration: 1s;
-      h1 {
-        position: absolute;
-        bottom: 30px;
-        left: 120px;
-        font-size: $font-size-amazing;
-        text-align: center;
-        margin-top: 50px;
-        color: $color-text-gray;
-        font-weight: bolder;
-        span {
-          font-size: $font-size-xl;
-        }
-      }
-      .cover {
-        position: absolute;
-        z-index: 2;
-        right: 62px;
-        bottom: 90px;
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-        overflow: hidden;
-        @include bg-image('~common/img/home');
-        background-size: contain;
+.home {
+  .content {
+    position: relative;
+    width: 1200px;
+    height: 400px;
+    margin: 0 auto;
+    animation-duration: 1s;
+    h1 {
+      position: absolute;
+      bottom: 30px;
+      left: 120px;
+      font-size: $font-size-amazing;
+      text-align: center;
+      margin-top: 50px;
+      color: $color-text-gray;
+      font-weight: bolder;
+      span {
+        font-size: $font-size-xl;
       }
     }
+    .cover {
+      position: absolute;
+      z-index: 2;
+      right: 62px;
+      bottom: 90px;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      overflow: hidden;
+      @include bg-image('~common/img/home');
+      background-size: contain;
+    }
   }
+}
 </style>
